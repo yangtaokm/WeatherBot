b3e4913cb37ad3adca8fd74041ac283093cd88e6
taoyang <taoyang@TAO-SURFACE>
2016-07-21 13:49:31 +0800

    Add project files.

---
+++ WeatherBot/Controllers/MessagesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace WeatherBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                // calculate something for us to return
+                string city = (activity.Text ?? string.Empty);
+
+                // return our reply to the user
+                string replyMsg = await GetWeather(city);
+                Activity reply = activity.CreateReply(replyMsg);
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+        private async Task<string> GetWeather(string strCity)
+
+        {
+
+            string strRet = string.Empty;
+            string strTemp = string.Empty;
+
+            WeatherData wd = new WeatherData();
+
+                wd = await WeatherService.GetWeatherfromService(strCity);
+
+
+            // return our reply to the user
+
+            if (null == wd)
+
+            {
+
+                strRet = string.Format("City {0} doesn't appear to be valid", strCity.ToUpper());
+
+            }
+
+            else
+
+            {
+
+
+                strRet = string.Format(" {0} now, {1}: {2} and {3} Degree", wd.city, wd.weathermain,wd.weatherdescription,wd.temp);
+
+            }
+
+
+
+            return strRet;
+
+        }
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
